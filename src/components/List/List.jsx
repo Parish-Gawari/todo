682a79a7a43200d956dfd028190d1bcf9cdabbe6
Samael-24 <parishk1024@gmail.com>
2023-08-28 17:28:52 +0530

    Pre-Final Version

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -4,13 +4,24 @@ import { Button } from "react-bootstrap";
 import styles from "./List.module.css"
 
 const List = ({tasks,isCheckHandler,isEditingHandler,cancelHandler,itemListChangeHandler,itemListDateChangeHandler,itemSaveHandler,deleteHandler}) => {
-  const listItems = tasks.map((task,index)=>(
 
-        <div key={index} className="border border-primary-subtle rounded p-2 w-sm-25">
+  const delayDate = (data)=>{
+    const day1 = new Date(data)
+    const day2 = new Date()
+    return(day1.toISOString() < day2.toISOString())
+    
+  }
+  
+  // style={delayDate(task.dueDate)}
+  
+ const listItems = tasks.map((task,index)=>(
+
+        <div className="w-sm-25">
+        <div key={index} className= "rounded p-3 w-sm-25" style={(!task.isDone && delayDate(task.dueDate)) ? {border: "2px solid yellow"}: {border: "2px solid aqua"}} >
           {!task.isEditing && (
             <>
-            <p>{task.item}</p>
-            <p>Due Date: {task.dueDate} </p>
+            <p style={task.isDone ? {textDecoration: "line-through"}: {}}>{task.item}</p>
+            <p style={task.isDone ? {textDecoration: "line-through"}: {}}>Due Date: {task.dueDate} </p>
             </>
           )}
           {task.isEditing && (
@@ -20,8 +31,9 @@ const List = ({tasks,isCheckHandler,isEditingHandler,cancelHandler,itemListChang
             <input type="date" value={task.editDate} className="p-1" onChange={(val)=>itemListDateChangeHandler(index,val.target.value)}/>
             </div>
             <div className="d-flex justify-content-center mt-2 mb-1">
-            <Button variant="transparent" onClick={()=>itemSaveHandler(index)}>Save</Button>
-            <Button variant="transparent" onClick={()=>cancelHandler(index)}>Cancel</Button>
+            <Button variant="transparent" onClick={()=>itemSaveHandler(index)} id={styles.Save}>Save</Button>
+            <Button variant="transparent" onClick={()=>cancelHandler(index)} id={styles.Cancel} >Cancel</Button>
+            
             </div>
             </>
           )}
@@ -30,6 +42,12 @@ const List = ({tasks,isCheckHandler,isEditingHandler,cancelHandler,itemListChang
             <Button variant="transparent" className="border-none" onClick={() => isEditingHandler(index)} disabled={task.isDone === true} id={styles.Edit}>Edit</Button>
             <Button variant="transparent" id={styles.delete} onClick={()=>deleteHandler(index)}>Delete</Button>
           </div>
+        </div>
+        {(!task.isDone && delayDate(task.dueDate)) && (
+          <>
+          <p className="text-center text-danger" style={{fontSize: "12px"}}>*Due Day Has Been Passed</p>
+          </>
+        )}
         </div>
        
   ));
